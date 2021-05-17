d2b1b3e2f57190b1fc76f5cd0c0dba6f4a599763
RazanAl-Quran <r.alquran@ltuc.com>
2021-05-17 11:37:05 +0300

    lab01

---
+++ src/components/Main.js
@@ -0,0 +1,36 @@
+import React from 'react';
+import HornedBeast from './HornedBeast';
+import hornsData from './hornsData.json'
+
+
+class Main extends React.Component {
+  render(){
+    return(
+      <>
+        {hornsData.map(item=>{
+            return (
+                <HornedBeast
+                    title={item.title}
+                    imgUrl={item.image_url}
+                    description= {item.description}
+                />
+            )
+        })}
+      </>
+    )
+  }
+}
+
+export default Main;
+
+
+  {/* <HornedBeast
+            title={'UniWhal'}
+            imgUrl={'http://3.bp.blogspot.com/_DBYF1AdFaHw/TE-f0cDQ24I/AAAAAAAACZg/l-FdTZ6M7z8/s1600/Unicorn_and_Narwhal_by_dinglehopper.jpg'}
+            description='A unicorn and a narwhal nuzzling their horns'
+        />
+        <HornedBeast
+            title={'aaaaaaaaaaa'}
+            imgUrl={'http://3.bp.blogspot.com/_DBYF1AdFaHw/TE-f0cDQ24I/AAAAAAAACZg/l-FdTZ6M7z8/s1600/Unicorn_and_Narwhal_by_dinglehopper.jpg'}
+            description='A unssssssssssssssssssss'
+        /> */}
