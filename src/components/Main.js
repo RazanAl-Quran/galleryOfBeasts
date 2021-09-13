d62cad9219ffc15e8c0777ba4f8d1104dd886bbe
RazanAl-Quran <r.alquran@ltuc.com>
2021-09-13 11:29:21 +0300

    cc

--- src/components/Main.js
+++ src/components/Main.js
@@ -10,7 +10,7 @@ class Main extends React.Component {
         {hornsData.map((item,idx)=>{
             return (
                 <HornedBeast
-                
+                    key = {idx}
                     title={item.title}
                     imgUrl={item.image_url}
                     description= {item.description}
@@ -24,14 +24,3 @@ class Main extends React.Component {
 
 export default Main;
 
-
-  {/* <HornedBeast
-            title={'UniWhal'}
-            imgUrl={'http://3.bp.blogspot.com/_DBYF1AdFaHw/TE-f0cDQ24I/AAAAAAAACZg/l-FdTZ6M7z8/s1600/Unicorn_and_Narwhal_by_dinglehopper.jpg'}
-            description='A unicorn and a narwhal nuzzling their horns'
-        />
-        <HornedBeast
-            title={'aaaaaaaaaaa'}
-            imgUrl={'http://3.bp.blogspot.com/_DBYF1AdFaHw/TE-f0cDQ24I/AAAAAAAACZg/l-FdTZ6M7z8/s1600/Unicorn_and_Narwhal_by_dinglehopper.jpg'}
-            description='A unssssssssssssssssssss'
-        /> */}
